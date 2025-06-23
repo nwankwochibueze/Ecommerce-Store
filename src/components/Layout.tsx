55a87e4b6adc160c0c6f146a70e6b2b805217cff
Nwankwo Chibueze <nbarnestine1@gmail.com>
2025-06-23 18:24:29 +0100

    Add Footer, Layout, and PageNotFound components

---
+++ src/components/Layout.tsx
@@ -0,0 +1,20 @@
+import React, { Suspense } from "react";
+import { Outlet } from "react-router-dom";
+import NavMenu from "./NavMenu";
+import Footer from "./Footer";
+
+const Layout: React.FC = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <NavMenu />
+      <main className="flex-grow">
+        <Suspense fallback={<div>Loading...</div>}>
+          <Outlet />
+        </Suspense>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
