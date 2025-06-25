432ffe852adfc3a95f891f35226dd15d3dc69ac1
Nwankwo Chibueze <nbarnestine1@gmail.com>
2025-06-25 12:40:59 +0100

    Add/update Cloudinary API and ShopProductsSection component

---
+++ src/components/ShopProductsSection.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ShopProductsSection: React.FC = () => {
+  return (
+    <div>
+      <div>
+        <h2>Our Best Sellers</h2>
+        <p>Dont Miss Out.</p>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default ShopProductsSection;
