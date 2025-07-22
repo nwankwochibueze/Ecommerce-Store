858bb19c9c521142105e26ee5db62a6c5bfb1295
Nwankwo Chibueze <nbarnestine1@gmail.com>
2025-07-22 16:47:59 +0100

    refactor: update modal logic, type safety

--- src/store/Type.ts
+++ src/store/Type.ts
@@ -1,4 +1,3 @@
-// src/types.ts
 export interface CartProduct {
   _id: string;
   title: string;
@@ -8,4 +7,12 @@ export interface CartProduct {
   selectedSize: string;
   selectedColor: string;
   quantity: number;
+}
+
+export type SanityImage = {
+  _type: 'image'
+  asset: {
+    _ref: string
+    _type: 'reference'
+  }
 }
